6e37ab375530ec5b547894db92c37e8b6ee131ad
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2018-11-15 20:04:34 -0500

    php additon

---
+++ action_page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE php>
+<html lang="en-us">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <title>Joshua LaRochelle Portfolio</title>
+    <link rel="icon" href="assets/images/icon.png" type="image/gif" sizes="16x16">
+</head>
+<body>
+
+Thank you, <?php echo $_GET["name"]; ?>!<br>
+Your message of: <?php echo $_GET["email"]; ?> will be read my Josh soon.
+
+</body>
+</html>
